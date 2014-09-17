fea4d1789fae2f31eb4ac71393737fb0da95cae7
Leo Dion <leogdion@brightdigit.com>
2014-09-17 14:30:20 -0400

    compiles with reference to google gmail server

---
+++ sincestory/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sincestory
+//
+//  Created by Leo G Dion on 9/17/14.
+//  Copyright (c) 2014 BrightDigit, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  @IBAction func click(sender:AnyObject) {
+    var service = GTLServiceGmail()
+  }
+}
+
---
+++ sincestory/bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  sincestory
+//
+//  Created by Leo G Dion on 9/17/14.
+//  Copyright (c) 2014 BrightDigit, LLC. All rights reserved.
+//
+
+#ifndef sincestory_bridge_h
+#define sincestory_bridge_h
+
+#import "GTLGmail.h"
+
+#endif
